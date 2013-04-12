b696ae10136844779b86fb361d165404a9221b9e
Max Willembrinck <maxwills@gmail.com>
2013-04-12 15:29:27 -0300

    Create script_pixel_ping_db.sql

---
+++ pixel_ping/script_pixel_ping_db.sql
@@ -0,0 +1,14 @@
+delimiter $$
+
+CREATE TABLE `pixel_stats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(255) DEFAULT NULL,
+  `hits` int(11) DEFAULT NULL,
+  `datetime_ts` datetime DEFAULT NULL,
+  `day_of_the_month` int(11) DEFAULT NULL,
+  `month_of_the_year` int(11) DEFAULT NULL,
+  `year` year(4) DEFAULT NULL,
+  `week_of_the_year` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
+
