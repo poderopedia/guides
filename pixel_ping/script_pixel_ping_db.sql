0c426beda38847a0482ddbf277302e5b17ebb6c2
Juan Eduardo <juaneduardohv@gmail.com>
2013-04-22 13:17:44 -0300

    date_field index correction & utf8

--- pixel_ping/script_pixel_ping_db.sql
+++ pixel_ping/script_pixel_ping_db.sql
@@ -1,6 +1,6 @@
 delimiter $$
 
-CREATE TABLE `pixel_stats` (
+CREATE TABLE  `pixel_test`.`pixel_stats` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `page_key` varchar(255) DEFAULT NULL,
   `hits` int(11) DEFAULT NULL,
@@ -9,6 +9,12 @@ CREATE TABLE `pixel_stats` (
   `month_of_the_year` int(11) DEFAULT NULL,
   `year` year(4) DEFAULT NULL,
   `week_of_the_year` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
+  `date_day` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_day` (`day_of_the_month`),
+  KEY `idx_month` (`month_of_the_year`),
+  KEY `idx_year` (`year`),
+  KEY `idx_week` (`week_of_the_year`),
+  KEY `idx_date` (`date_day`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8$$
 
